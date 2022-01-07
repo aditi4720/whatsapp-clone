cae76d9df1557bd16f3ff6d178f82f7bad499241
aditi4720 <singh.aditi9698@gmail.com>
2022-01-08 00:38:18 +0530

    feat: Added chat feature

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,57 @@
+import { useState, useEffect } from "react";
 import React from "react";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
 import "./chat.css";
 function Chat() {
-  return <div className="chat"></div>;
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/pixel-art/${seed}.svg`}
+        ></Avatar>
+        <div className="chat_headerInfo">
+          <h3>Person Name</h3>
+          <p>Last Seen</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchIcon></SearchIcon>
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon></AttachFileIcon>
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon></MoreVertIcon>
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className={`chat_message ${true && "chat_reciever"}`}>
+          <span className="chat_name">Aditi </span>
+          hey babe
+          <span className="chat_timestamp">12:39pm</span>
+        </p>
+      </div>
+      <div className="chat_footer">
+        <InsertEmoticonIcon></InsertEmoticonIcon>
+        <form>
+          <input type="text" />
+          <button>Send a message</button>
+        </form>
+        <MicIcon></MicIcon>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
